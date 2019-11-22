2a84b777145fca2d8d6d42bfc20e7ab35ce1801d
Nikhil Vijayan <nkhilv@gmail.com>
2019-11-22 00:56:21 +0000

    :lipstick: change output style

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const argsArray = process.argv;
 const arg = argsArray[2];
 
 if (data[arg]) {
-  console.log(chalk.hex('#f07c59')(data[arg].headline));
+  console.log(chalk.hex('#43e86f').inverse(data[arg].headline));
   console.log(chalk.hex('#d2f5d0')(data[arg].summary));
 } else {
   console.log('nothing yet...');
